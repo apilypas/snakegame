5c53f4bb1cc645962ed708d2ada3296a97c89dae
apilypas <andrius.pilypas@gmail.com>
2025-03-09 02:23:37 +0200

    Minor fix related to tiled map drawing

--- src/SnakeGame.DesktopGL/Content/Map.tsx
+++ src/SnakeGame.DesktopGL/Content/Map.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Tiles" tilewidth="16" tileheight="16" tilecount="8" columns="6">
+<tileset version="1.10" tiledversion="1.11.2" name="Tiles" tilewidth="16" tileheight="16" tilecount="18" columns="6">
  <grid orientation="orthogonal" width="16" height="16"/>
  <image source="Tiles.png" width="96" height="48"/>
 </tileset>
